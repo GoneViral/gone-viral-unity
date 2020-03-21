7fcf75b26dfea5ddcec1a53d200d69f407a35088
Noah <minimops.noah@gmail.com>
2020-03-21 18:21:55 +0100

    controller layout

--- Assets/Scripts/GameLogic.cs
+++ Assets/Scripts/GameLogic.cs
@@ -1,20 +1,29 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Net.Mime;
 using UnityEngine;
 using NDream.AirConsole;
 using Newtonsoft.Json.Linq;
 
 public class GameLogic : MonoBehaviour
 {
+    
     void Awake()
     {
         AirConsole.instance.onMessage += OnMessage;
+        
     }
 
     void OnMessage(int fromDeviceID, JToken data){
         Debug.Log("message from " + fromDeviceID + ", data: " + data);
-        if(data["action"] != null && data["action"].ToString().Equals("interact")){
-            Camera.main.backgroundColor = new Color(Random.Range(0f, 1f), Random.Range(0f,1f), Random.Range(0f, 1f));
+        if(data["action"] != null && data["action"].ToString().Equals("action"))
+        {
+            Camera.main.backgroundColor = Color.green;
+        }
+        else if (data["action"] != null && data["action"].ToString().Equals("switch"))
+        
+        {
+            Camera.main.backgroundColor = Color.red;
         }
     }
 
